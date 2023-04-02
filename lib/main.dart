a07e23f62a25393f9fac71eaf13acdc0e60dd63c
itunes2412 <itunes2412@gmail.com>
2023-04-01 21:22:50 -0600

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,52 @@ class MyApp extends StatelessWidget {
       title: '3.4. Utilización de widgets',
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 137, 0, 222),
-          title: const Text('PELICULAS'),
+          backgroundColor: const Color.fromARGB(255, 137, 0, 222),
+          title: const Text('PELICULAS EN 4K'),
         ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+        body: Stack(
           children: [
-            ContainerAmarillo(
-              width: 300,
-              height: 60,
-              texto: "Pulp Fiction",
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                      'https://images.unsplash.com/photo-1616530940355-351fabd9524b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-            ContainerAmarillo(
-              width: 300,
-              height: 60,
-              texto: "Una odisea del espacio",
-            ),
-            ContainerAmarillo(
-              width: 300,
-              height: 60,
-              texto: "La lista de Schindler",
-            ),
-            ContainerAmarillo(
-              width: 300,
-              height: 60,
-              texto: "Regreso al futuro",
-            ),
-            ContainerAmarillo(
-              width: 300,
-              height: 60,
-              texto: "Apocalypse Now",
+            const Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  ContainerAmarillo(
+                    width: 300,
+                    height: 60,
+                    texto: "Pulp Fiction",
+                  ),
+                  ContainerAmarillo(
+                    width: 300,
+                    height: 60,
+                    texto: "Una odisea del espacio",
+                  ),
+                  ContainerAmarillo(
+                    width: 300,
+                    height: 60,
+                    texto: "La lista de Schindler",
+                  ),
+                  ContainerAmarillo(
+                    width: 300,
+                    height: 60,
+                    texto: "Regreso al futuro",
+                  ),
+                  ContainerAmarillo(
+                    width: 300,
+                    height: 60,
+                    texto: "Apocalypse Now",
+                  ),
+                ],
+              ),
             ),
           ],
         ),
@@ -70,7 +86,7 @@ class ContainerAmarillo extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Center(
-        child: Text(texto, style: TextStyle(fontSize: 24)),
+        child: Text(texto, style: TextStyle(fontSize: 25)),
       ),
     );
   }
